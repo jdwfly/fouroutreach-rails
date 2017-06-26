b0d66ac30017343f4a4f40addf3494f69e4705d5
Joseph Wheaton <joseph.wheaton@gmail.com>
2017-06-25 21:46:30 -0700

    Add basic integration tests for Prospects

--- test/controllers/prospects_controller_test.rb
+++ test/controllers/prospects_controller_test.rb
@@ -2,16 +2,11 @@ require 'test_helper'
 
 class ProspectsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @prospect = Prospect.create!(name: "Clark Kent")
+    @prospect = prospects(:tom)
   end
 
   test "should get index" do
     get prospects_path
-    assert_response :success
-  end
-
-  test "should get show" do
-    get prospect_path(@prospect)
     assert_response :success
   end
 
@@ -20,4 +15,54 @@ class ProspectsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create prospect" do
+    assert_difference('Prospect.count') do
+      post prospects_url, params: { prospect: { name: @prospect.name,
+                                                address: @prospect.address,
+                                                city: @prospect.city,
+                                                state: @prospect.state,
+                                                zip: @prospect.zip,
+                                                phone: @prospect.phone,
+                                                email: @prospect.email,
+                                                point_of_contact: @prospect.point_of_contact,
+                                                interests: @prospect.interests,
+                                                comments: @prospect.comments,
+                                                date_first_contact: @prospect.date_first_contact } }
+    end
+
+    assert_redirected_to prospect_url(Prospect.last)
+  end
+
+  test "should show prospect" do
+    get prospect_url(@prospect)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_prospect_url(@prospect)
+    assert_response :success
+  end
+
+  test "should update vendor" do
+    patch prospect_url(@prospect), params: { prospect: { name: @prospect.name,
+                                              address: @prospect.address,
+                                              city: @prospect.city,
+                                              state: @prospect.state,
+                                              zip: @prospect.zip,
+                                              phone: @prospect.phone,
+                                              email: @prospect.email,
+                                              point_of_contact: @prospect.point_of_contact,
+                                              interests: @prospect.interests,
+                                              comments: @prospect.comments,
+                                              date_first_contact: @prospect.date_first_contact } }
+    assert_redirected_to prospect_url(@prospect)
+  end
+
+  test "should destroy vendor" do
+    assert_difference('Prospect.count', -1) do
+      delete prospect_url(@prospect)
+    end
+
+    assert_redirected_to prospects_url
+  end
 end
