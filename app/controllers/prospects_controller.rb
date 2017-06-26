5ecc22f2583e4008ae62a3fe477217adf9760cfe
Joseph Wheaton <joseph.wheaton@gmail.com>
2017-06-25 20:54:58 -0700

    Add all methods for Prospects

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -8,6 +8,49 @@ class ProspectsController < ApplicationController
   end
 
   def new
-
+    @prospect = Prospect.new
   end
+
+  def create
+    @prospect = User.new(prospect_params)
+    if @prospect.save
+      flash[:success] = "Prospect successfully created!"
+      redirect_to @prospect
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @prospect.update_attributes(prosepct_params)
+      flash[:success] = "Prospect information updated!"
+      redirect_to @prospect
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Prospect.find(params[:id]).destroy
+    flash[:success] = "Prospect deleted!"
+    redirect_to prospects_url
+  end
+
+  private
+    def prospect_params
+      params.require(:prospect).permit(:name,
+                                       :address,
+                                       :city,
+                                       :state,
+                                       :zip,
+                                       :phone,
+                                       :email,
+                                       :point_of_contact,
+                                       :interests,
+                                       :comments,
+                                       :date_first_contact)
+    end
 end
