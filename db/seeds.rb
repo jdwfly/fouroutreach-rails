3da6a9c90318269c135ec99c53877877854f1d29
Joseph Wheaton <joseph.wheaton@gmail.com>
2017-06-27 11:09:35 -0700

    Update seed.rb for dev and test cases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-testuser = User.create!(email: "joe@example.com",
-                    password: "joeblow",
-                    password_confirmation: "joeblow")
-10.times do |n|
+user1 = User.create!(email: "user1@example.com",
+                    password: "password",
+                    password_confirmation: "password")
+user2 = User.create!(email: "user2@example.com",
+                     password: "password",
+                     password_confirmation: "password")
+# For user1
+5.times do |n|
   name = Faker::Name.name
   address = Faker::Address.street_address
   city = Faker::Address.city
@@ -11,7 +15,28 @@ testuser = User.create!(email: "joe@example.com",
   email = "example-#{n+1}@josephwheaton.com"
   poc = "Visitation"
   interests = Faker::Hacker.ingverb
-  testuser.prospects.create!(name: name,
+  user1.prospects.create!(name: name,
+                   address: address,
+                   city: city,
+                   state: state,
+                   zip: zip,
+                   phone: phone,
+                   email: email,
+                   point_of_contact: poc,
+                   interests: interests)
+end
+# For user2
+5.times do |n|
+  name = Faker::Name.name
+  address = Faker::Address.street_address
+  city = Faker::Address.city
+  state = Faker::Address.state_abbr
+  zip = Faker::Address.zip
+  phone = Faker::PhoneNumber.phone_number
+  email = "example-#{n+10}@josephwheaton.com"
+  poc = "Visitation"
+  interests = Faker::Hacker.ingverb
+  user2.prospects.create!(name: name,
                    address: address,
                    city: city,
                    state: state,
